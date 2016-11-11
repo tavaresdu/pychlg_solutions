8ec207efcd30e03dd68360378b72e7d2c2a1d10d
Eduardo Tavares <eduardo.tavares@cortex-intelligence.com>
2016-11-11 17:56:07 -0200

    Solução 4 finalizada

---
+++ 04.py
@@ -0,0 +1,19 @@
+from urllib import request
+import time
+import re
+
+nothing = 12345
+
+url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php'
+
+while(True):
+    with request.urlopen(url + '?nothing=' + str(nothing)) as response:
+        text = response.read().decode('utf-8')
+        print(text)
+        match = re.search(r'next nothing is (\d+)', text, re.MULTILINE)
+        if match is not None:
+            nothing = int(match.group(1))
+        elif 'Divide by two' in text:
+            nothing = nothing // 2
+        elif 'html' in text:
+            break
